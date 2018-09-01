f8cff9040e287aa8d56fe5763b2e6d225a54f830
shaun.sharples@gmail.com <shaun.sharples@gmail.com>
2018-09-02 00:52:51 +0200

    create window and logical device

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,17 +13,18 @@ import           Control.Exception
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Managed
--- import           Data.Bits
+import           Data.Bits
+import qualified Data.Set                               as Set
 import           Foreign.C.String
 import           Foreign.Marshal.Alloc
 import           Foreign.Marshal.Array
--- import           Foreign.Ptr
+import           Foreign.Ptr
 import           Foreign.Storable
 import qualified Graphics.UI.GLFW                       as GLFW
 import           Graphics.Vulkan
 import           Graphics.Vulkan.Core_1_0
--- import           Graphics.Vulkan.Ext.VK_KHR_surface
--- import           Graphics.Vulkan.Ext.VK_KHR_swapchain
+import           Graphics.Vulkan.Ext.VK_KHR_surface
+import           Graphics.Vulkan.Ext.VK_KHR_swapchain
 import           Graphics.Vulkan.Marshal.Create
 import           Safe
 
@@ -34,13 +35,29 @@ test :: IO ()
 test = runManaged $ do
   glfw
   glfwReqExts <- requiredGLFWExtensions
-  vkInstance <- using $ vulkan "vulkan-test"
-                               "vulkan-test"
-                               glfwReqExts
-                               ["VK_LAYER_LUNARG_standard_validation"]
-  logMsg "Initialized"
-  device <- liftIO $ pickPhysicalDevice vkInstance
+  vkInstance  <- vulkanInstance
+                   "vulkan-test"
+                   "vulkan-test"
+                   glfwReqExts
+                   layers
+  window      <- windowTitled "vulkan-test"
+  surface     <- windowSurface vkInstance window
+  (pDevice, queues)
+              <- pickPhysicalDevice vkInstance extensions
+  graphicsFamily
+              <- findGraphicsQueueFamilyIndex queues
+  presentFamily
+              <- findPresentQueueFamilyIndex vkInstance pDevice queues
+  device      <- logicalDevice
+                   pDevice
+                   extensions
+                   layers
+                   graphicsFamily
+
   return ()
+  where
+    extensions = [VK_KHR_SWAPCHAIN_EXTENSION_NAME]
+    layers = ["VK_LAYER_LUNARG_standard_validation"]
 
 glfw :: Managed ()
 glfw =
@@ -69,36 +86,22 @@ requiredGLFWExtensions = liftIO $ do
   logMsg $ "GLFW requires extensions: " ++ unwords extNames
   return extensions
 
-vulkan
+vulkanInstance
   :: String
   -> String
   -> [CString]
   -> [String]
   -> Managed VkInstance
-vulkan progName engineName extensions layers =
+vulkanInstance progName engineName extensions layers =
   managed $
-    withVulkanInstance
-      progName
-      engineName
-      extensions
-      layers
-
-withVulkanInstance
-  :: String
-  -> String
-  -> [CString]
-  -> [String]
-  -> (VkInstance -> IO a)
-  -> IO a
-withVulkanInstance progName engineName extensions layers =
-  bracket
-    ( createVulkanInstance
-        progName
-        engineName
-        extensions
-        layers
-    )
-    destroyVulkanInstance
+    bracket
+      ( createVulkanInstance
+          progName
+          engineName
+          extensions
+          layers
+      )
+      destroyVulkanInstance
 
 allocaPeek
   :: Storable a
@@ -145,7 +148,6 @@ destroyVulkanInstance vkInstance =
   vkDestroyInstance vkInstance VK_NULL
     <* logMsg "Destroyed Vulkan instance"
 
-{-
 fetchAll
   :: (Storable a, Storable b, Integral a)
   => (Ptr a -> Ptr b -> IO ())
@@ -158,7 +160,6 @@ fetchAll f =
     allocaArray devCount $ \arrayPtr -> do
       f countPtr arrayPtr
       peekArray devCount arrayPtr
--}
 
 fetchAllMsg
   :: (Storable a, Storable b, Integral a)
@@ -176,47 +177,128 @@ fetchAllMsg msg f =
         >>= throwVkResult msg
       peekArray devCount arrayPtr
 
-pickPhysicalDevice :: VkInstance -> IO VkPhysicalDevice
-pickPhysicalDevice vkInstance = do
+pickPhysicalDevice
+  :: MonadIO m
+  => VkInstance
+  -> [CString]
+  -> m (VkPhysicalDevice, [VkQueueFamilyProperties])
+pickPhysicalDevice vkInstance requiredExtensions = liftIO $ do
+  logMsg "Enumerating Vulkan devices"
   allDevices <-
     fetchAllMsg
       "pickPhysicalDevice: Failed to enumerate physical devices."
-      (vkEnumeratePhysicalDevices vkInstance )
+      (vkEnumeratePhysicalDevices vkInstance)
 
-  devices <- forM (zip [1..] allDevices) $ \(n::Int, device) -> do
-    logMsg $ "---- Device #" ++ show n ++ " ----"
+  devices <- forM (zip [1..] allDevices) $ \(devIndex, device) -> do
+    logMsg $ "---- Device #" ++ show devIndex ++ " ----"
     props <- physicalDeviceProperties device
     logDeviceProperties props
     feats <- physicalDeviceFeatures device
     logDeviceFeatures feats
+    queues <- physicalDeviceQueueFamiliesProps device
+    extensions <- enumerateDeviceExtensionProperties device
+    logDeviceExtensionProperties extensions
     logMsg "-------------------"
-    return (device, props, feats)
+    return (devIndex, device, props, feats, queues, extensions)
 
-  logMsg $ "Number of devices found " ++ show (length devices)
+  logMsg $ "Number of devices found: " ++ show (length devices)
 
-  gpus <- filterM isDeviceSuitable devices
+  gpus <- filterM (isDeviceSuitable requiredExtensions) devices
 
-  logMsg $ "Number of GPU devices found " ++ show (length gpus)
+  logMsg $ "Number of GPU devices found: " ++ show (length gpus)
 
   case headMay gpus of
-    Just (dev, _, _) -> return dev
-    Nothing -> throwVkMsg "No suitable GPU devices!"
+    Just (devIndex, dev, _, _, queues, _) -> do
+      logMsg ("Picked device #" ++ show devIndex)
+      return (dev, queues)
+    Nothing ->
+      throwVkMsg "No suitable GPU devices!"
 
-isDeviceSuitable :: (VkPhysicalDevice, VkPhysicalDeviceProperties, VkPhysicalDeviceFeatures) -> IO Bool
-isDeviceSuitable (_, props, feats) =
-  return $ isGpu && hasGeomShader
+isDeviceSuitable
+  :: [CString]
+  -> ( Word32
+     , VkPhysicalDevice
+     , VkPhysicalDeviceProperties
+     , VkPhysicalDeviceFeatures
+     , [VkQueueFamilyProperties]
+     , [VkExtensionProperties]
+     )
+  -> IO Bool
+isDeviceSuitable requiredExtensions (_, _, props, feats, queues, extensions) = do
+  hasExtensions <- supportsRequiredExtensions
+  return $ isGpu && hasGeomShader && hasGraphicsQueueFamily && hasExtensions
   where
     isGpu =
-      case getField @"deviceType" props of
-        VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU   -> True
-        VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU -> True
-        VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU    -> True
-        _                                      -> False
+      getField @"deviceType" props `elem`
+        [ VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
+        , VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU
+        , VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU
+        ]
 
     hasGeomShader =
       case getField @"geometryShader" feats of
         VK_TRUE -> True
         _       -> False
+
+    hasGraphicsQueueFamily =
+      any isGraphicsQueueFamily queues
+
+    supportsRequiredExtensions = do
+      req <- Set.fromList <$> mapM peekCString requiredExtensions
+      let exts = Set.fromList $ map extensionName extensions
+      return $ req `Set.isSubsetOf` exts
+
+isGraphicsQueueFamily :: VkQueueFamilyProperties -> Bool
+isGraphicsQueueFamily queue =
+  getField @"queueFlags" queue .&. VK_QUEUE_GRAPHICS_BIT /= zeroBits
+
+enumerateDeviceExtensionProperties
+  :: VkPhysicalDevice
+  -> IO [VkExtensionProperties]
+enumerateDeviceExtensionProperties physicalDevice =
+  fetchAllMsg
+    "enumerateDeviceExtensionProperties: Failed to enumerate extensions."
+    (vkEnumerateDeviceExtensionProperties physicalDevice VK_NULL)
+
+logDeviceExtensionProperties :: [VkExtensionProperties] -> IO ()
+logDeviceExtensionProperties extensions =
+  logMsg $ unwords [ "Device Extensions:", unwords (map extensionName extensions)]
+
+extensionName :: VkExtensionProperties -> String
+extensionName = getStringField @"extensionName"
+
+findGraphicsQueueFamilyIndex
+  :: MonadIO m
+  => [VkQueueFamilyProperties]
+  -> m Word32
+findGraphicsQueueFamilyIndex queues = liftIO $ do
+  let families = filter (isGraphicsQueueFamily . snd) (zip [0..] queues)
+  case headMay families of
+    Just (queueFamilyIndex, _) -> do
+      logMsg ("Picked graphics queue family #" ++ show queueFamilyIndex)
+      return queueFamilyIndex
+    Nothing ->
+      throwVkMsg "No suitable graphics queue family!"
+
+findPresentQueueFamilyIndex
+  :: MonadIO m
+  => VkInstance
+  -> VkPhysicalDevice
+  -> [VkQueueFamilyProperties]
+  -> m Word32
+findPresentQueueFamilyIndex vkInstance physicalDevice queues = liftIO $ do
+  families <- filterM (isPresentQueueFamily . fst) (zip [0..] queues)
+  case headMay families of
+    Just (queueFamilyIndex, _) -> do
+      logMsg ("Picked present queue family #" ++ show queueFamilyIndex)
+      return queueFamilyIndex
+    Nothing ->
+      throwVkMsg "No suitable present queue family!"
+  where
+    isPresentQueueFamily =
+      GLFW.getPhysicalDevicePresentationSupport
+        vkInstance
+        physicalDevice
 
 physicalDeviceProperties :: VkPhysicalDevice -> IO VkPhysicalDeviceProperties
 physicalDeviceProperties = allocaPeek . vkGetPhysicalDeviceProperties
@@ -253,6 +335,120 @@ logDeviceFeatures feats = do
       case getField @fname feats of
         VK_TRUE -> "Yes"
         _ -> "No"
+
+physicalDeviceQueueFamiliesProps :: VkPhysicalDevice -> IO [VkQueueFamilyProperties]
+physicalDeviceQueueFamiliesProps = fetchAll . vkGetPhysicalDeviceQueueFamilyProperties
+
+logicalDevice
+  :: VkPhysicalDevice
+  -> [CString]
+  -> [String]
+  -> Word32
+  -> Managed VkDevice
+logicalDevice physicalDevice extensions layers queueFamilyIndex =
+  managed $
+    bracket
+      ( createLogicalDevice
+          physicalDevice
+          extensions
+          layers
+          queueFamilyIndex
+      )
+      destroyDevice
+
+createLogicalDevice
+  :: VkPhysicalDevice
+  -> [CString]
+  -> [String]
+  -> Word32
+  -> IO VkDevice
+createLogicalDevice physicalDevice extensions layers queueFamilyIndex =
+  withPtr createInfo $ \ciPtr ->
+    allocaPeek
+    ( vkCreateDevice physicalDevice ciPtr VK_NULL
+        >=> throwVkResult "vkCreateDevice: Failed to create device."
+    )
+    <* logMsg "Created logical device"
+  where
+    qCreateInfo = createVk @VkDeviceQueueCreateInfo
+      $  set        @"sType" VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO
+      &* set        @"pNext" VK_NULL
+      &* set        @"flags" 0
+      &* set        @"queueFamilyIndex" queueFamilyIndex
+      &* set        @"queueCount" 1
+      &* setListRef @"pQueuePriorities" [1.0]
+
+    createInfo = createVk @VkDeviceCreateInfo
+      $  set           @"sType" VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO
+      &* set           @"pNext" VK_NULL
+      &* set           @"flags" 0
+      &* setListRef    @"pQueueCreateInfos" [ qCreateInfo ]
+      &* set           @"queueCreateInfoCount" 1
+      &* set           @"enabledLayerCount" (fromIntegral $ length layers)
+      &* setStrListRef @"ppEnabledLayerNames" layers
+      &* set           @"enabledExtensionCount" (fromIntegral $ length extensions)
+      &* setListRef    @"ppEnabledExtensionNames" extensions
+      &* set           @"pEnabledFeatures" VK_NULL
+
+destroyDevice :: VkDevice -> IO ()
+destroyDevice device =
+  vkDestroyDevice device VK_NULL
+    <* logMsg "Destroyed logical device"
+
+windowSurface
+  :: VkInstance
+  -> GLFW.Window
+  -> Managed VkSurfaceKHR
+windowSurface vkInstance window =
+  managed $
+    bracket
+      ( createSurface
+          vkInstance
+          window
+      )
+      ( destroySurface
+          vkInstance
+      )
+
+createSurface :: VkInstance -> GLFW.Window -> IO VkSurfaceKHR
+createSurface vkInstance window =
+  allocaPeek
+    ( GLFW.createWindowSurface
+        vkInstance
+        window
+        nullPtr
+          >=> throwVkResult "createSurfaceWindow: Failed to create surface."
+    )
+    <* logMsg "Created surface"
+
+
+destroySurface :: VkInstance -> VkSurfaceKHR -> IO ()
+destroySurface vkInstance surface = do
+  vkDestroySurfaceKHR vkInstance surface VK_NULL
+  logMsg "Destroyed surface"
+
+windowTitled :: String -> Managed GLFW.Window
+windowTitled title =
+  managed $
+    bracket (createWindow title) destroyWindow
+
+createWindow :: String -> IO GLFW.Window
+createWindow title = do
+   -- We don't require OpenGL context
+  GLFW.windowHint $ GLFW.WindowHint'ClientAPI GLFW.ClientAPI'NoAPI
+
+  maybeWindow <- GLFW.createWindow 640 360 title Nothing Nothing
+  case maybeWindow of
+    Nothing ->
+      throwIO $ GlfwException "createWindow: Failed to create window."
+    Just window -> do
+      logMsg "Created window"
+      return window
+
+destroyWindow :: GLFW.Window -> IO ()
+destroyWindow window = do
+  GLFW.destroyWindow window
+  logMsg "Destroyed window"
 
 newtype GlfwException
   = GlfwException
