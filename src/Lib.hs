c634ec6e4ebc7efba33b654e468593c31dfd6b47
shaun.sharples@gmail.com <shaun.sharples@gmail.com>
2018-09-02 11:57:08 +0200

    create a swapchain

--- src/Lib.hs
+++ src/Lib.hs
@@ -40,10 +40,10 @@ test = runManaged $ do
                    "vulkan-test"
                    glfwReqExts
                    layers
-  window      <- windowTitled "vulkan-test"
+  window      <- windowTitled winWidth winHeight "vulkan-test"
   surface     <- windowSurface vkInstance window
-  (pDevice, queues)
-              <- pickPhysicalDevice vkInstance extensions
+  (pDevice, queues, surfaceCaps, surfaceFormats, presentModes)
+              <- pickPhysicalDevice vkInstance surface extensions
   graphicsFamily
               <- findGraphicsQueueFamilyIndex queues
   presentFamily
@@ -53,11 +53,22 @@ test = runManaged $ do
                    extensions
                    layers
                    graphicsFamily
-
+  chain       <- pickSwapchain
+                   device
+                   graphicsFamily
+                   presentFamily
+                   surface
+                   surfaceCaps
+                   surfaceFormats
+                   presentModes
+                   winWidth
+                   winHeight
   return ()
   where
     extensions = [VK_KHR_SWAPCHAIN_EXTENSION_NAME]
     layers = ["VK_LAYER_LUNARG_standard_validation"]
+    winWidth = 640
+    winHeight = 360
 
 glfw :: Managed ()
 glfw =
@@ -180,9 +191,10 @@ fetchAllMsg msg f =
 pickPhysicalDevice
   :: MonadIO m
   => VkInstance
+  -> VkSurfaceKHR
   -> [CString]
-  -> m (VkPhysicalDevice, [VkQueueFamilyProperties])
-pickPhysicalDevice vkInstance requiredExtensions = liftIO $ do
+  -> m (VkPhysicalDevice, [VkQueueFamilyProperties], VkSurfaceCapabilitiesKHR, [VkSurfaceFormatKHR], [VkPresentModeKHR])
+pickPhysicalDevice vkInstance surface requiredExtensions = liftIO $ do
   logMsg "Enumerating Vulkan devices"
   allDevices <-
     fetchAllMsg
@@ -198,8 +210,24 @@ pickPhysicalDevice vkInstance requiredExtensions = liftIO $ do
     queues <- physicalDeviceQueueFamiliesProps device
     extensions <- enumerateDeviceExtensionProperties device
     logDeviceExtensionProperties extensions
+    surfaceCaps <- getPhysicalDeviceSurfaceCapabilities device surface
+    logSurfaceCapabilities surfaceCaps
+    surfaceFormats <- getPhysicalDeviceSurfaceFormats device surface
+    logSurfaceFormats surfaceFormats
+    presentModes <- getPhysicalDeviceSurfacePresentModes device surface
+    logPresentModes presentModes
     logMsg "-------------------"
-    return (devIndex, device, props, feats, queues, extensions)
+    return
+      ( devIndex
+      , device
+      , props
+      , feats
+      , queues
+      , extensions
+      , surfaceCaps
+      , surfaceFormats
+      , presentModes
+      )
 
   logMsg $ "Number of devices found: " ++ show (length devices)
 
@@ -208,9 +236,9 @@ pickPhysicalDevice vkInstance requiredExtensions = liftIO $ do
   logMsg $ "Number of GPU devices found: " ++ show (length gpus)
 
   case headMay gpus of
-    Just (devIndex, dev, _, _, queues, _) -> do
+    Just (devIndex, dev, _, _, queues, _, surfaceCaps, surfaceFormats, presentModes) -> do
       logMsg ("Picked device #" ++ show devIndex)
-      return (dev, queues)
+      return (dev, queues, surfaceCaps, surfaceFormats, presentModes)
     Nothing ->
       throwVkMsg "No suitable GPU devices!"
 
@@ -222,11 +250,28 @@ isDeviceSuitable
      , VkPhysicalDeviceFeatures
      , [VkQueueFamilyProperties]
      , [VkExtensionProperties]
+     , VkSurfaceCapabilitiesKHR
+     , [VkSurfaceFormatKHR]
+     , [VkPresentModeKHR]
      )
   -> IO Bool
-isDeviceSuitable requiredExtensions (_, _, props, feats, queues, extensions) = do
+isDeviceSuitable requiredExtensions
+    ( _
+    , _
+    , props
+    , feats
+    , queues
+    , extensions
+    , surfaceCaps
+    , surfaceFormats
+    , presentModes
+    ) = do
   hasExtensions <- supportsRequiredExtensions
-  return $ isGpu && hasGeomShader && hasGraphicsQueueFamily && hasExtensions
+  return $ isGpu
+        && hasGeomShader
+        && hasGraphicsQueueFamily
+        && hasExtensions
+        && hasSuitableSwapChain
   where
     isGpu =
       getField @"deviceType" props `elem`
@@ -248,6 +293,11 @@ isDeviceSuitable requiredExtensions (_, _, props, feats, queues, extensions) = d
       let exts = Set.fromList $ map extensionName extensions
       return $ req `Set.isSubsetOf` exts
 
+    hasSuitableSwapChain =
+      getField @"minImageCount" surfaceCaps >= 2 &&
+      (not . null) surfaceFormats &&
+      (not . null) presentModes
+
 isGraphicsQueueFamily :: VkQueueFamilyProperties -> Bool
 isGraphicsQueueFamily queue =
   getField @"queueFlags" queue .&. VK_QUEUE_GRAPHICS_BIT /= zeroBits
@@ -266,6 +316,52 @@ logDeviceExtensionProperties extensions =
 
 extensionName :: VkExtensionProperties -> String
 extensionName = getStringField @"extensionName"
+
+getPhysicalDeviceSurfaceCapabilities
+  :: VkPhysicalDevice
+  -> VkSurfaceKHR
+  -> IO VkSurfaceCapabilitiesKHR
+getPhysicalDeviceSurfaceCapabilities physicalDevice surface =
+  allocaPeek
+    ( vkGetPhysicalDeviceSurfaceCapabilitiesKHR physicalDevice surface
+        >=> throwVkResult "vkGetPhysicalDeviceSurfaceCapabilitiesKHR: Failed to get surface capabilities"
+    )
+
+logSurfaceCapabilities :: VkSurfaceCapabilitiesKHR -> IO ()
+logSurfaceCapabilities caps =
+  logMsg $ unwords [ "Surface Capabilities:", unwords [minImageCount, maxImageCount] ]
+  where
+    minImageCount = "minImageCount=" ++ show (getField @"minImageCount" caps)
+    maxImageCount = "minImageCount=" ++ show (getField @"maxImageCount" caps)
+
+getPhysicalDeviceSurfaceFormats
+  :: VkPhysicalDevice
+  -> VkSurfaceKHR
+  -> IO [VkSurfaceFormatKHR]
+getPhysicalDeviceSurfaceFormats physicalDevice surface =
+  fetchAllMsg "vkGetPhysicalDeviceSurfaceFormatsKHR: Failed to get surface formats"
+    (vkGetPhysicalDeviceSurfaceFormatsKHR physicalDevice surface)
+
+logSurfaceFormats :: [VkSurfaceFormatKHR] -> IO ()
+logSurfaceFormats formats =
+  logMsg $ unwords [ "Surface Formats:", unwords (map showFormat formats)]
+  where
+    showFormat f =
+      show (getField @"format" f)
+      ++ "/"
+      ++ show (getField @"colorSpace" f)
+
+getPhysicalDeviceSurfacePresentModes
+  :: VkPhysicalDevice
+  -> VkSurfaceKHR
+  -> IO [VkPresentModeKHR]
+getPhysicalDeviceSurfacePresentModes physicalDevice surface =
+  fetchAllMsg "vkGetPhysicalDeviceSurfacePresentModesKHR: Failed to get surface present modes"
+    (vkGetPhysicalDeviceSurfacePresentModesKHR physicalDevice surface)
+
+logPresentModes :: [VkPresentModeKHR] -> IO ()
+logPresentModes presentModes =
+  logMsg $ unwords [ "Surface Present Modes:", unwords (map show presentModes)]
 
 findGraphicsQueueFamilyIndex
   :: MonadIO m
@@ -427,17 +523,24 @@ destroySurface vkInstance surface = do
   vkDestroySurfaceKHR vkInstance surface VK_NULL
   logMsg "Destroyed surface"
 
-windowTitled :: String -> Managed GLFW.Window
-windowTitled title =
+windowTitled :: Word32 -> Word32 -> String -> Managed GLFW.Window
+windowTitled width height title =
   managed $
-    bracket (createWindow title) destroyWindow
+    bracket (createWindow width height title) destroyWindow
 
-createWindow :: String -> IO GLFW.Window
-createWindow title = do
+createWindow :: Word32 -> Word32 -> String -> IO GLFW.Window
+createWindow width height title = do
    -- We don't require OpenGL context
   GLFW.windowHint $ GLFW.WindowHint'ClientAPI GLFW.ClientAPI'NoAPI
 
-  maybeWindow <- GLFW.createWindow 640 360 title Nothing Nothing
+  maybeWindow <-
+    GLFW.createWindow
+      (fromIntegral width)
+      (fromIntegral height)
+      title
+      Nothing
+      Nothing
+
   case maybeWindow of
     Nothing ->
       throwIO $ GlfwException "createWindow: Failed to create window."
@@ -449,6 +552,121 @@ destroyWindow :: GLFW.Window -> IO ()
 destroyWindow window = do
   GLFW.destroyWindow window
   logMsg "Destroyed window"
+
+pickSwapchain
+  :: VkDevice
+  -> Word32
+  -> Word32
+  -> VkSurfaceKHR
+  -> VkSurfaceCapabilitiesKHR
+  -> [VkSurfaceFormatKHR]
+  -> [VkPresentModeKHR]
+  -> Word32
+  -> Word32
+  -> Managed VkSwapchainKHR
+pickSwapchain
+    device
+    graphicsFamily
+    presentFamily
+    surface
+    surfaceCaps
+    surfaceFormats
+    presentModes
+    width
+    height = do
+  (surfaceFormat, colorSpace) <- liftIO $ pickSurfaceFormat surfaceFormats
+  presentMode <- liftIO $ pickPresentMode presentModes
+  extent <- liftIO $ pickSwapExtent surfaceCaps width height
+  imageCount <- liftIO $ pickImageCount surfaceCaps
+  logMsg
+    ( "Picked swapchain: " ++ unwords
+      [ "format=" ++ show surfaceFormat
+      , "colorspace=" ++ show colorSpace
+      , "mode=" ++ show presentMode
+      , "extent=" ++ show extent
+      , "imagecount=" ++ show imageCount
+      ]
+    )
+  swapchain
+    device
+    graphicsFamily
+    presentFamily
+    surface
+    surfaceFormat
+    colorSpace
+    presentMode
+    imageCount
+    extent
+    (getField @"currentTransform" surfaceCaps)
+
+pickSurfaceFormat :: [VkSurfaceFormatKHR] -> IO (VkFormat, VkColorSpaceKHR)
+pickSurfaceFormat surfaceFormats =
+  if anyFormat || hasFormat VK_FORMAT_B8G8R8A8_UNORM VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+  then
+    return (VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+  else
+    return $ formatPair (head surfaceFormats)
+  where
+    formatPair sf = (getField @"format" sf, getField @"colorSpace" sf)
+    anyFormat = length surfaceFormats == 1 && getField @"format" (head surfaceFormats) == VK_FORMAT_UNDEFINED
+    hasFormat f cs = any (\sf -> getField @"format" sf == f && getField @"colorSpace" sf == cs) surfaceFormats
+
+pickPresentMode :: [VkPresentModeKHR] -> IO VkPresentModeKHR
+pickPresentMode presentModes = go preference
+  where
+    go [] =
+      case headMay presentModes of
+        Just mode -> return mode
+        Nothing -> throwVkMsg "No suitable present modes!"
+
+    go (pref : rest)
+      | pref `elem` presentModes = return pref
+      | otherwise = go rest
+
+    preference =
+      [ VK_PRESENT_MODE_MAILBOX_KHR
+      , VK_PRESENT_MODE_FIFO_KHR
+      , VK_PRESENT_MODE_FIFO_RELAXED_KHR
+      , VK_PRESENT_MODE_IMMEDIATE_KHR
+      ]
+
+pickSwapExtent
+  :: VkSurfaceCapabilitiesKHR
+  -> Word32
+  -> Word32
+  -> IO VkExtent2D
+pickSwapExtent surfaceCaps width height = do
+  let currentExtent = getField @"currentExtent" surfaceCaps
+      w = getField @"width" currentExtent
+      h = getField @"height" currentExtent
+  if (w, h) == (maxBound, maxBound)
+  then do
+    let minExtent = getField @"minImageExtent" surfaceCaps
+        maxExtent = getField @"maxImageExtent" surfaceCaps
+        wMin = getField @"width" minExtent
+        wMax = getField @"width" maxExtent
+        hMin = getField @"height" minExtent
+        hMax = getField @"height" maxExtent
+    return $
+      createVk @VkExtent2D
+        $  set @"width" (clamp wMin wMax width)
+        &* set @"height" (clamp hMin hMax height)
+  else
+    return currentExtent
+  where
+    clamp mn mx = min mx . max mn
+
+pickImageCount
+  :: VkSurfaceCapabilitiesKHR
+  -> IO Word32
+pickImageCount surfaceCaps = do
+  let cMin = getField @"minImageCount" surfaceCaps
+      cMax = getField @"maxImageCount" surfaceCaps    -- 0 means no max limit
+  if cMin >= 3 || cMax == cMin
+  then
+    return cMin
+  else
+    return (cMin + 1)
 
 newtype GlfwException
   = GlfwException
@@ -462,6 +680,108 @@ instance Exception GlfwException where
     , "Vulkan exception:"
     , "*** " ++ msg
     ]
+
+swapchain
+  :: VkDevice
+  -> Word32
+  -> Word32
+  -> VkSurfaceKHR
+  -> VkFormat
+  -> VkColorSpaceKHR
+  -> VkPresentModeKHR
+  -> Word32
+  -> VkExtent2D
+  -> VkSurfaceTransformFlagBitsKHR
+  -> Managed VkSwapchainKHR
+swapchain
+    device
+    graphicsFamily
+    presentFamily
+    surface
+    surfaceFormat
+    colorSpace
+    presentMode
+    imageCount
+    extent
+    transform =
+  managed $
+    bracket
+      ( createSwapchain
+          device
+          graphicsFamily
+          presentFamily
+          surface
+          surfaceFormat
+          colorSpace
+          presentMode
+          imageCount
+          extent
+          transform
+       )
+       ( destroySwapchain device )
+
+createSwapchain
+  :: VkDevice
+  -> Word32
+  -> Word32
+  -> VkSurfaceKHR
+  -> VkFormat
+  -> VkColorSpaceKHR
+  -> VkPresentModeKHR
+  -> Word32
+  -> VkExtent2D
+  -> VkSurfaceTransformFlagBitsKHR
+  -> IO VkSwapchainKHR
+createSwapchain
+    device
+    graphicsFamily
+    presentFamily
+    surface
+    surfaceFormat
+    colorSpace
+    presentMode
+    imageCount
+    extent
+    transform =
+  withPtr createInfo $ \ciPtr ->
+    allocaPeek
+    ( vkCreateSwapchainKHR device ciPtr VK_NULL
+        >=> throwVkResult "vkCreateSwapchainKHR: Failed to create swapchain."
+    )
+    <* logMsg "Created swapchain"
+  where
+    createInfo = createVk @VkSwapchainCreateInfoKHR
+      $  set        @"sType" VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR
+      &* set        @"pNext" VK_NULL
+      &* set        @"surface" surface
+      &* set        @"minImageCount" imageCount
+      &* set        @"imageFormat" surfaceFormat
+      &* set        @"imageColorSpace" colorSpace
+      &* set        @"imageExtent" extent
+      &* set        @"imageArrayLayers" 1
+      &* set        @"imageUsage" VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT
+      &* set        @"preTransform" transform
+      &* set        @"compositeAlpha" VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR
+      &* set        @"presentMode" presentMode
+      &* set        @"clipped" VK_TRUE
+      &* set        @"oldSwapchain" VK_NULL
+      &* if graphicsFamily == presentFamily
+         then
+              set        @"imageSharingMode" VK_SHARING_MODE_EXCLUSIVE
+           &* set        @"queueFamilyIndexCount" 0
+           &* set        @"pQueueFamilyIndices" VK_NULL
+         else
+              set        @"imageSharingMode" VK_SHARING_MODE_CONCURRENT
+           &* set        @"queueFamilyIndexCount" 2
+           &* setListRef @"pQueueFamilyIndices" [graphicsFamily, presentFamily]
+
+destroySwapchain
+  :: VkDevice
+  -> VkSwapchainKHR
+  -> IO ()
+destroySwapchain device chain =
+  vkDestroySwapchainKHR device chain VK_NULL
+    <* logMsg "Destroyed swapchain"
 
 throwGLFW :: MonadIO m => String -> Bool -> m ()
 throwGLFW msg bool = liftIO $
